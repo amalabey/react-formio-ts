ac24f40e6319a2d59848f64e204123e6c6294122
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-01-29 14:33:41 +1100

    Added two more react attributes to typings

--- types/react-formio/index.d.ts
+++ types/react-formio/index.d.ts
@@ -1,6 +1,14 @@
 declare module "react-formio" {
     import * as React from "react";
     
-    class Formio extends React.Component<any, any> { 
+    class FormioProps {
+        src: string;
+        form?: any;
+        submission?: any;
+        onChange?(submission: any, key: string, value: string): void;
+        onFormSubmit?(submission: any): void;
+    }
+
+    class Formio extends React.Component<FormioProps, any> { 
     }
 }
